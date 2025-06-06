d8a5ea4d18636c194541054c1488297120f9d931
Samuel MICHAUX <samuel.michaux@ik.me>
2025-06-06 11:24:43 +0200

    start security | userdetails & service

---
+++ src/main/java/com/formation/events/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.formation.events.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.formation.events.entities.UserEntity;
+import com.formation.events.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    UserEntity user = userRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("Utilisateur inconnu avec cet email : " + username));
+
+    return CustomUserPrincipal.create(user);
+  }
+
+}
---
+++ src/main/java/com/formation/events/security/CustomUserPrincipal.java
@@ -0,0 +1,50 @@
+package com.formation.events.security;
+
+import java.util.Collection;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.formation.events.entities.UserEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CustomUserPrincipal implements UserDetails {
+
+  private Long id;
+  private String email;
+  private String password;
+  private Collection<? extends GrantedAuthority> authorities;
+
+  public static CustomUserPrincipal create(UserEntity user) {
+    List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+
+    return new CustomUserPrincipal(
+        user.getId(),
+        user.getEmail(),
+        user.getPassword(),
+        authorities);
+  }
+
+  @Override
+  public String getPassword() {
+    return this.password;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.email;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return this.authorities;
+  }
+
+}
